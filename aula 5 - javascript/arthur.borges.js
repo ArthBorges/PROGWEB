a32124a2754ac4b96f1ebdf33427d23c98bb0480
Arthur Borges <arthurba03@gmail.com>
2024-10-14 20:30:11 -0400

    Add files via upload

---
+++ aula 5 - javascript/arthur.borges.js
@@ -0,0 +1,9 @@
+function mudarCor() {
+	const div = document.getElementById("quadrado");
+	div.style["background-color"] = "red";
+}
+
+function retornar_cor() {
+    const div = document.getElementById("quadrado");
+	div.style["background-color"] = "gray";
+}
